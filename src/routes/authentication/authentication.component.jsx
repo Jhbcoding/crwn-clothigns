d7be4cbf281bb93842314d00d16928c62fd6e5bb
Jatinsha <jhbstart793@gmail.com>
2023-06-14 15:08:43 +0530

    section one to eleven completed

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,15 @@
+import SignUpForm from "../../components/sing-up-form/sign-up-form.component";
+import SignInForm from "../../components/sing-in-form/sign-in-form.component";
+
+import { AuthenticationContainer } from "./authentication.styles";
+
+const Authentication = () => {
+  return (
+    <AuthenticationContainer>
+      <SignInForm />
+      <SignUpForm />
+    </AuthenticationContainer>
+  );
+};
+
+export default Authentication;
